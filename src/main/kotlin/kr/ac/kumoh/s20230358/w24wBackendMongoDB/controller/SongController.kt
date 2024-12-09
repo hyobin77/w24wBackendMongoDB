4d4030414b4b241d2686f09f0163761c63c02792
hyobin77 <gyqls0509@kumoh.ac.kr>
2024-12-09 13:44:37 +0900

    🚀 Backend 완성

--- src/main/kotlin/kr/ac/kumoh/s20230358/w24wBackendMongoDB/controller/SongController.kt
+++ src/main/kotlin/kr/ac/kumoh/s20230358/w24wBackendMongoDB/controller/SongController.kt
@@ -5,7 +5,28 @@ import kr.ac.kumoh.s20230358.w24wBackendMongoDB.service.SongService
 import org.springframework.web.bind.annotation.*
 
 @RestController
+@RequestMapping("/api/songs")
 class SongController(private val service: SongService) {
-    @GetMapping("/api/songs")
+    @PostMapping
+    fun addSong(@RequestBody song: Song): Song = service.addSong(song)
+
+    @GetMapping
     fun getAllSongs(): List<Song> = service.getAllSongs()
+
+    @GetMapping("/{id}")
+    fun getSongById(@PathVariable id: String): Song? = service.getSongById(id)
+
+    @GetMapping("/singer/{singer}")
+    fun getSongBySinger(@PathVariable singer: String): List<Song> = service.getSongBySinger(singer)
+
+    @PutMapping("/{id}")
+    fun updateSong(@PathVariable id: String, @RequestBody songDetails: Song): Song? =  service.updateSong(id, songDetails)
+
+    @DeleteMapping("/{id}")
+    fun deleteSong(@PathVariable id: String): Map<String, String> {
+        return if (service.deleteSong(id))
+            mapOf("status" to "deleted")
+        else
+            mapOf("status" to "not found")
+    }
 }
--- src/main/kotlin/kr/ac/kumoh/s20230358/w24wBackendMongoDB/service/SongService.kt
+++ src/main/kotlin/kr/ac/kumoh/s20230358/w24wBackendMongoDB/service/SongService.kt
@@ -6,5 +6,36 @@ import org.springframework.stereotype.Service
 
 @Service
 class SongService(private val repository: SongRepository) {
+    fun addSong(song: Song): Song = repository.save(song)
+
     fun getAllSongs(): List<Song> = repository.findAll()
+    fun getSongById(id: String): Song? = repository.findById(id).orElse(null)
+    fun getSongBySinger(singer: String): List<Song> = repository.findBySinger(singer)
+
+    fun updateSong(id: String, song: Song): Song? {
+        val songTarget = repository.findById(id)
+
+        return if (songTarget.isPresent) {
+            val oldSong = songTarget.get()
+            val updatedSong = oldSong.copy(
+                title = song.title,
+                singer = song.singer,
+                rating = song.rating,
+                lyrics = song.lyrics
+            )
+            repository.save(updatedSong)
+        } else {
+            null
+        }
+    }
+
+    fun deleteSong(id: String): Boolean {
+        return if (repository.existsById(id)) {
+            repository.deleteById(id)
+            true
+        } else {
+            false
+        }
+    }
+
 }
