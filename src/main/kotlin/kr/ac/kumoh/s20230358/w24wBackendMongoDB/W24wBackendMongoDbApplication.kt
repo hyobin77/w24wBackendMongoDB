6f85d06cc84175786bdaa62cb34d968e391e8390
hyobin77 <gyqls0509@kumoh.ac.kr>
2024-12-09 13:21:36 +0900

    Initial commit

---
+++ src/main/kotlin/kr/ac/kumoh/s20230358/w24wBackendMongoDB/W24wBackendMongoDbApplication.kt
@@ -0,0 +1,11 @@
+package kr.ac.kumoh.s20230358.w24wBackendMongoDB
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class W24wBackendMongoDbApplication
+
+fun main(args: Array<String>) {
+	runApplication<W24wBackendMongoDbApplication>(*args)
+}
---
+++ src/test/kotlin/kr/ac/kumoh/s20230358/w24wBackendMongoDB/W24wBackendMongoDbApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.ac.kumoh.s20230358.w24wBackendMongoDB
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class W24wBackendMongoDbApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
